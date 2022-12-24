432aed7382cb237b78b60c5f058a3a61308aa30b
Lorenzo Murarotto <lnzmrr@gmail.com>
2022-12-24 17:21:57 +0000

    add polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { fileURLToPath, URL } from "node:url";
+import Legacy from "@vitejs/plugin-legacy";
 import Vue from "@vitejs/plugin-vue";
-import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
-import { defineConfig } from "vite";
+
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 import { IconComponentResolver } from "./icon-component-resolver";
+
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     Vue(),
+    Legacy(),
     Components({
       resolvers: [NaiveUiResolver(), IconComponentResolver({ pkg: "@vicons/fa", prefix: "Fa" })],
       dirs: "src",
